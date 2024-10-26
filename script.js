8ab2c65eb255f86ae05d15ad1dd46d5877fe8ca9
nabeel7555 <nabeelpips@gmail.com>
2024-10-26 16:23:56 +0500

    Initial Files

---
+++ script.js
@@ -0,0 +1,20 @@
+function add() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const result = num1 + num2;
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+function subtract() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const result = num1 - num2;
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+function multiply() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const result = num1 * num2;
+    document.getElementById("result").innerText = "Result: " + result;
+}
